cc23bfb45a4d73ec454c746e9f9b3e0e2ded2ab3
Nick Kane <nkanedev@gmail.com>
2024-01-12 11:20:59 -0500

    chore(plugin): update treesitter configuration settings

--- lua/nkane/lazy/treesitter.lua
+++ lua/nkane/lazy/treesitter.lua
@@ -3,8 +3,22 @@ return {
     build = ":TSUpdate",
     config = function()
         require('nvim-treesitter.configs').setup({
+            ensure_isntalled ={
+                "c",
+                "cpp",
+                "go",
+                "javascript",
+                "typescript",
+                "lua",
+            },
             sync_install = true,
-            auto_install = true
+            auto_install = true,
+            highlight = {
+                enabled = true
+            },
+            indent = {
+                enabled = true
+            }
         })
     end
 }
