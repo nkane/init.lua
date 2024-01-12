4a948ce888af31cf9440b89bb57ae326baedfbcf
Nick Kane <nkanedev@gmail.com>
2024-01-12 09:15:56 -0500

    chore(colorschema): update color schema set hl color

--- lua/nkane/lazy/colorscheme.lua
+++ lua/nkane/lazy/colorscheme.lua
@@ -3,5 +3,11 @@ return {
     name = 'rose-pine',
     config = function()
         vim.cmd('colorscheme rose-pine')
+        require('rose-pine').setup({
+            disable_background = true
+        })
+	    vim.cmd.colorscheme('rose-pine')
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
 }
