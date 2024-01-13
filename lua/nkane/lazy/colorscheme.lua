5a74ac4193021d37f04b3ed1a6ab423fd685bd59
Nick Kane <nkanedev@gmail.com>
2024-01-13 10:00:57 -0500

    chore(theme): add catppuccin theme

--- lua/nkane/lazy/colorscheme.lua
+++ lua/nkane/lazy/colorscheme.lua
@@ -1,13 +1,20 @@
 return {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-        vim.cmd('colorscheme rose-pine')
-        require('rose-pine').setup({
-            disable_background = true
-        })
-	    vim.cmd.colorscheme('rose-pine')
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+            require('rose-pine').setup({
+                disable_background = true
+            })
+            vim.cmd.colorscheme('rose-pine')
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        end
+    },
+    {
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        priority = 1000
+    }
 }
