c263d09cf5cfd8d91ce76213d9d74115e004c9ed
Nick Kane <nkanedev@gmail.com>
2024-02-03 09:14:46 -0500

    chore: minor updates

--- lua/nkane/lazy/colorscheme.lua
+++ lua/nkane/lazy/colorscheme.lua
@@ -3,7 +3,7 @@ return {
         'rose-pine/neovim',
         name = 'rose-pine',
         config = function()
-            vim.cmd('colorscheme rose-pine')
+            -- vim.cmd('colorscheme rose-pine')
             require('rose-pine').setup({
                 disable_background = true
             })
@@ -11,10 +11,5 @@ return {
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         end
-    },
-    {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        priority = 1000
     }
 }
--- lua/nkane/lazy/none-ls.lua
+++ lua/nkane/lazy/none-ls.lua
@@ -9,6 +9,7 @@ return {
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
                 null_ls.builtins.formatting.eslint_d,
+                null_ls.builtins.formatting.gofumpt,
             }
         })
         vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
