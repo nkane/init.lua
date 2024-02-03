bee2ead544afa5d31306210a8c419eeec32e665c
Nick Kane <nkanedev@gmail.com>
2024-02-03 09:16:53 -0500

    chore: minor update to lualine theme

--- lua/nkane/lazy/colorscheme.lua
+++ lua/nkane/lazy/colorscheme.lua
@@ -3,7 +3,7 @@ return {
         'rose-pine/neovim',
         name = 'rose-pine',
         config = function()
-            -- vim.cmd('colorscheme rose-pine')
+            vim.cmd('colorscheme rose-pine')
             require('rose-pine').setup({
                 disable_background = true
             })
