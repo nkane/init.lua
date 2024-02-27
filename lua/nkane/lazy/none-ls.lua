84848bd63057214d13ec58df132200305d170ea3
Nick Kane <nkanedev@gmail.com>
2024-02-27 16:26:54 -0500

    chore: remove eslint_d from none-ls

--- lua/nkane/lazy/none-ls.lua
+++ lua/nkane/lazy/none-ls.lua
@@ -8,7 +8,6 @@ return {
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.eslint_d,
                 null_ls.builtins.formatting.gofumpt,
             }
         })
