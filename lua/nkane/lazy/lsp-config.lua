18f1c995452483eafe2d5d53ba8014859c3a1333
Nick Kane <nkanedev@gmail.com>
2024-06-29 21:29:36 -0400

    update: updates to lsp-config, none-ls, and telescope

--- lua/nkane/lazy/lsp-config.lua
+++ lua/nkane/lazy/lsp-config.lua
@@ -1,10 +1,10 @@
 return {
     {
-        'neovim/nvim-lspconfig',
+        "neovim/nvim-lspconfig",
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require('lspconfig')
-            local util = require('lspconfig/util')
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            local util = require("lspconfig/util")
 
             lspconfig.lua_ls.setup({
                 capabilities = capabilities,
@@ -12,18 +12,18 @@ return {
 
             lspconfig.gopls.setup({
                 capabilities = capabilities,
-                cmd = { 'gopls' },
-                filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
-                root_dir = util.root_pattern('go.work', 'go.mod', '.git')
+                cmd = { "gopls" },
+                filetypes = { "go", "gomod", "gowork", "gotmpl" },
+                root_dir = util.root_pattern("go.work", "go.mod", ".git"),
             })
 
             lspconfig.tsserver.setup({
                 capabilities = capabilities,
             })
 
-            lspconfig.eslint.setup({
-                capabilities = capabilities,
-            })
+            -- lspconfig.eslint.setup({
+            --     capabilities = capabilities,
+            -- })
 
             lspconfig.tailwindcss.setup({
                 capabilities = capabilities,
@@ -35,34 +35,31 @@ return {
 
             lspconfig.marksman.setup({
                 capabilities = capabilities,
-                filetypes = { 'md', 'markdown' },
+                filetypes = { "md", "markdown" },
             })
 
-            vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-                vim.lsp.handlers.hover,
-                {
-                    border = 'rounded'
-                }
-            )
+            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+                border = "rounded",
+            })
 
-            vim.keymap.set('n', 'gD', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, {})
-            vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, {})
-            vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "gD", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, {})
+            vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, {})
             --vim.keymap.set('n', '<leader>vf', vim.diagnostic.open_float, {})
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_next, {})
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, {})
-            vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, {})
-            vim.keymap.set('n', '<leader>vhh', vim.lsp.buf.signature_help, {})
-        end
+            vim.keymap.set("n", "[d", vim.diagnostic.goto_next, {})
+            vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, {})
+            vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, {})
+            vim.keymap.set("n", "<leader>vhh", vim.lsp.buf.signature_help, {})
+        end,
     },
     {
-        'ray-x/lsp_signature.nvim',
+        "ray-x/lsp_signature.nvim",
         config = function()
-            require('lsp_signature').setup({})
-        end
+            require("lsp_signature").setup({})
+        end,
     },
 }
--- lua/nkane/lazy/telescope.lua
+++ lua/nkane/lazy/telescope.lua
@@ -1,20 +1,20 @@
 return {
     {
-        'nvim-telescope/telescope-ui-select.nvim'
+        "nvim-telescope/telescope-ui-select.nvim",
     },
     {
-        'nvim-telescope/telescope.nvim',
+        "nvim-telescope/telescope.nvim",
         --tag = '0.1.5',
         dependencies = {
-            'plenary'
+            "plenary",
         },
         config = function()
-            require('telescope').setup({
+            require("telescope").setup({
                 extensions = {
                     ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {
+                        require("telescope.themes").get_dropdown({
                             -- even more opts
-                        }
+                        }),
                         -- pseudo code / specification for writing custom displays, like the one
                         -- for "codeactions"
                         -- specific_opts = {
@@ -28,20 +28,20 @@ return {
                         --      do the following
                         --   codeactions = false,
                         -- }
-                    }
-                }
+                    },
+                },
             })
-            require('telescope').load_extension('ui-select')
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>pz', builtin.current_buffer_fuzzy_find, {})
-            vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
-            vim.keymap.set('n', '<leader>ps', function()
-                builtin.grep_string({ search = vim.fn.input("Grep > ") });
+            require("telescope").load_extension("ui-select")
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+            vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>pz", builtin.current_buffer_fuzzy_find, {})
+            vim.keymap.set("n", "<leader>ht", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>ps", function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
             end)
-        end
-    }
+        end,
+    },
 }
