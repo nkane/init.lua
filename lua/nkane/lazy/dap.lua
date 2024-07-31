ae0c9ced544ee42711e3f8ed71c5e80bb1f13548
Nick Kane <nkanedev@gmail.com>
2024-07-31 12:53:28 -0400

    fix: typescript dap configuration

--- lua/nkane/lazy/dap.lua
+++ lua/nkane/lazy/dap.lua
@@ -105,55 +105,47 @@ return {
 				-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
 				debugger_path = "/home/nkane/.local/share/nvim/lazy/vscode-js-debug", -- Path to vscode-js-debug installation.
 				--debugger_cmd = { "js-debug-adapter" },                                           -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-				adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+				adapters = {
+					"pwa-node",
+					"pwa-chrome",
+					"pwa-msedge",
+					"node-terminal",
+					"pwa-extensionHost",
+					"node",
+					"chrome",
+				}, -- which adapters to register in nvim-dap
 				-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
 				-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-				-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+				log_console_level = vim.log.levels.ERROR, -- Logging level for output to console. Set to false to disable console output.
 			})
 
+			-- Configuration for TypeScript and JavaScript debugging
+			-- NOTE(nick): typescript debugging requires ts-node, make sure to install `npm i -g ts-node`
 			for _, language in ipairs({ "typescript", "javascript" }) do
 				require("dap").configurations[language] = {
 					{
 						type = "pwa-node",
 						request = "launch",
-						name = "Launch file",
-						program = "${file}",
-						cwd = "${workspaceFolder}",
-					},
-					{
-						type = "pwa-node",
-						request = "attach",
-						name = "Attach",
-						program = require("dap.utils").pick_process,
-						cwd = "${workspaceFolder}",
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Debug Jest Tests",
-						trace = true, -- include debugger info
-						runtimeExecutable = "node",
-						runtimeArgs = {
-							"../node_modules/jest/bin/jest.js",
-						},
-						rootPath = "${workspaceFolder}",
-						cwd = "${workspaceFolder}",
-						console = "integratedTerminal",
-						internalConsoleOptions = "neverOpen",
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch Test Current File (pwa-node with jest)",
+						name = "Launch Current File (pwa-node)",
 						cwd = vim.fn.getcwd(),
-						runtimeArgs = { "${workspaceFolder}/node_modules/.bin/jest" },
-						runtimeExecutable = "node",
-						args = { "${file}", "--coverage", "false" },
-						rootPath = "${workspaceFolder}",
+						args = { "${file}" },
 						sourceMaps = true,
-						console = "integratedTerminal",
-						internalConsoleOptions = "neverOpen",
+						protocol = "inspector",
+					},
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Launch Current File (pwa-node with ts-node)",
+						cwd = vim.fn.getcwd(),
+						runtimeExecutable = "ts-node",
+						args = { "${file}" },
+						sourceMaps = true,
+						protocol = "inspector",
 						skipFiles = { "<node_internals>/**", "node_modules/**" },
+						resolveSourceMapLocations = {
+							"${workspaceFolder}/**",
+							"!**/node_modules/**",
+						},
 					},
 				}
 			end
@@ -230,7 +222,6 @@ return {
 	},
 	{
 		"microsoft/vscode-js-debug",
-		opt = true,
-		run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+		build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
 	},
 }
