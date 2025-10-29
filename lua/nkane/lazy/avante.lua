514d92d2263aa37115f77d1a82435ea54142f923
Nick Kane <nkanedev@gmail.com>
2025-10-29 10:04:00 -0400

    fix: restructure avante config to separate copilot.lua plugin for proper initialization order

--- lua/nkane/lazy/avante.lua
+++ lua/nkane/lazy/avante.lua
@@ -1,8 +1,57 @@
 return {
   {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = true,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>"
+          },
+          layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
+      })
+    end,
+  },
+  {
     "yetone/avante.nvim",
     event = "VeryLazy",
-    lazy = false,
     version = false, -- Never set this value to "*"! Never!
     opts = {
       provider = "copilot"
@@ -11,6 +60,7 @@ return {
     build = "make",
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
+      "zbirenbaum/copilot.lua",
       "nvim-treesitter/nvim-treesitter",
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
@@ -21,56 +71,6 @@ return {
       "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
       "ibhagwan/fzf-lua",              -- for file_selector provider fzf
       "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-      {
-        "zbirenbaum/copilot.lua",        -- for providers='copilot'
-        cmd = "Copilot",
-        event = "InsertEnter",
-        config = function()
-          require("copilot").setup({
-            panel = {
-              enabled = true,
-              auto_refresh = false,
-              keymap = {
-                jump_prev = "[[",
-                jump_next = "]]",
-                accept = "<CR>",
-                refresh = "gr",
-                open = "<M-CR>"
-              },
-              layout = {
-                position = "bottom", -- | top | left | right
-                ratio = 0.4
-              },
-            },
-            suggestion = {
-              enabled = true,
-              auto_trigger = true,
-              debounce = 75,
-              keymap = {
-                accept = "<M-l>",
-                accept_word = false,
-                accept_line = false,
-                next = "<M-]>",
-                prev = "<M-[>",
-                dismiss = "<C-]>",
-              },
-            },
-            filetypes = {
-              yaml = false,
-              markdown = false,
-              help = false,
-              gitcommit = false,
-              gitrebase = false,
-              hgcommit = false,
-              svn = false,
-              cvs = false,
-              ["."] = false,
-            },
-            copilot_node_command = 'node', -- Node.js version must be > 18.x
-            server_opts_overrides = {},
-          })
-        end,
-      },
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
